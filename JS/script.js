70557161f65c71af86906c807e432d0d81ee27a5
Vladislav <vlad.goy23@gmail.com>
2022-07-09 17:18:45 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,43 @@
+ "use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+ //console.log(result);   
+
+ //const answer = +prompt("Are you 18?","");
+ //console.log(answer+5);   
+
+//const answers = [];
+//answers [0]=prompt('What is you name?', '');
+//answers [1]=prompt('What is you last name?', '');
+//answers [2]=prompt('How old are you?', ''); 
+
+//document.write(answers); 
+
+//console.log(typeof(answers));
+
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`);
+//const user = 'Vlad'
+//alert(`Hello,${user}`);
+
+let incr = 10,
+    decr = 10;
+    incr++;
+    decr--;
+    console.log(incr);
+    console.log(decr);
+
+    console.log(5%2);
+    console.log(2*4 === 8);
+    //*my* == string, === number;
+    // && = и;(2 true) || = или(хоча б 1 true);
+    // ! = отрицания
+    // != = не равно
+    
+
+
+
+
+    
